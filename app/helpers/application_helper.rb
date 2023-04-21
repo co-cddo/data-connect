ab1c60e0bb1135cd171fa052c26c6883c5d1ba07
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2023-04-21 15:55:35 +0100

    Initial bare rails app

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Default Helper for application wide helper methods
+module ApplicationHelper
+end
---
+++ app/models/application_record.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Root Active Record model object
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
+end
